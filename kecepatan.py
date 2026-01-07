36eda83c9089cbb4c230632c925e51544f90b209
Diveeeeee <akubanana2009@gmail.com>
2026-01-07 20:11:24 +0700

    Add files via upload

---
+++ kecepatan.py
@@ -0,0 +1,17 @@
+def kecepatan():
+  print("isi info2 ini, berikan - jika tidak ada")
+  jarak = input("masukkan jarak(meter): ")
+  waktu = input("masukkan waktu(second): ")
+  kecepatan = input("masukkan kecepatan(m/s): ")
+  
+  if jarak == "-":
+    hasil = int(kecepatan) * int(waktu)
+  if waktu == "-":
+    hasil = int(jarak) / int(kecepatan)
+  if kecepatan == "-":
+    hasil = int(jarak) / int(waktu)
+  print(hasil)
+  
+  
+  
+  
