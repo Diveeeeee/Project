3b4f607a3f01bf98d61baa75796a5b56617ed8df
Diveeeeee <akubanana2009@gmail.com>
2026-01-08 14:18:21 +0700

    Update kecepatan.py

--- kecepatan.py
+++ kecepatan.py
@@ -1,17 +1,15 @@
-def kecepatan():
-  print("isi info2 ini, berikan - jika tidak ada")
-  jarak = input("masukkan jarak(meter): ")
-  waktu = input("masukkan waktu(second): ")
-  kecepatan = input("masukkan kecepatan(m/s): ")
+print("isi info2 ini, berikan - jika tidak ada")
+jarak = input("masukkan jarak(meter): ")
+waktu = input("masukkan waktu(second): ")
+kecepatan = input("masukkan kecepatan(m/s): ")
   
-  if jarak == "-":
-    hasil = int(kecepatan) * int(waktu)
-  if waktu == "-":
-    hasil = int(jarak) / int(kecepatan)
-  if kecepatan == "-":
-    hasil = int(jarak) / int(waktu)
-  print(hasil)
-kecepatan()
+if jarak == "-":
+  hasil = int(kecepatan) * int(waktu)
+if waktu == "-":
+  hasil = int(jarak) / int(kecepatan)
+if kecepatan == "-":
+  hasil = int(jarak) / int(waktu)
+print(hasil)
   
   
   
