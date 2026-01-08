9a48d97f40ba590926bfd153292697d6dcb77afe
Diveeeeee <akubanana2009@gmail.com>
2026-01-08 14:17:51 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,15 +1,13 @@
-def kalkulator():
-  operasi = input("masukkan operasi: ")
-  b = operasi.split()
-  c = int(b[0])
-  d = int(b[2])
-  if b[1] == "+":
-    hasil = c + d 
-  if b[1] == "-":
-    hasil = c - d 
-  if b[1] == "×":
-    hasil = c * d 
-  if b[1] == "÷":
-    hasil = c / d 
-  print(hasil)
-kalkulator()
+operasi = input("masukkan operasi: ")
+b = operasi.split()
+c = int(b[0])
+d = int(b[2])
+if b[1] == "+":
+  hasil = c + d 
+if b[1] == "-":
+  hasil = c - d 
+if b[1] == "×":
+  hasil = c * d 
+if b[1] == "÷":
+  hasil = c / d 
+print(hasil)
